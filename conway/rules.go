6c5caa2158ac14b67f18d61c9737620e00cc3eff
Kamil Duda <11457257+kamil-duda@users.noreply.github.com>
2025-08-26 22:06:52 +0200

    #7 Rename functions

--- conway/rules.go
+++ conway/rules.go
@@ -1,6 +1,6 @@
 package conway
 
-func checkLiveCell(neighbours uint8) bool {
+func LiveCellSurvives(neighbours uint8) bool {
 	switch neighbours {
 	case 2, 3:
 		return true
@@ -9,7 +9,7 @@ func checkLiveCell(neighbours uint8) bool {
 	}
 }
 
-func checkDeadCell(neighbours uint8) bool {
+func DeadCellRevives(neighbours uint8) bool {
 	if neighbours == 3 {
 		return true
 	} else {
